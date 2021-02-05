94e1839dc51ac0e5abf4be7d692347a0ff457903
josedavid220 <joseocampo220@gmail.com>
2021-02-04 20:04:06 -0500

    Added a commentary to the code

--- Sorting/Insertion sort.py
+++ Sorting/Insertion sort.py
@@ -1,8 +1,8 @@
 # Written by Jose Ocampo
-# Insertion sort function increasing 
-# It recieves an numpy array x of numbers 
-# It returns the array x sorted in incresing order 
-# Example of usage: 
+# Insertion sort function increasing
+# It recieves an numpy array x of numbers
+# It returns the array x sorted in incresing order
+# Example of usage:
 # x = [5, 2, 1, 3, 4, 6]
 # increasing(x)
 # output: [1, 2, 3, 4, 5, 6]
@@ -10,9 +10,9 @@
 def increasing(x):
     for j in range(1,len(x)):
         key = x[j]
-        i = j-1
+        i = j-1 #dummy index
         while(i >= 0 and x[i] > key):
             x[i+1] = x[i]
             i = i-1
         x[i+1] = key
-    return x 
+    return x
